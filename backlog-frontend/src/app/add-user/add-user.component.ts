1a7941b648d27c38e5ec789d01be11e33321fcaf
William Collins <williamcollins@C02DQ8A9ML7H.attlocal.net>
2021-03-16 15:45:32 -0400

    Home page does not redirect until a valid user is found

--- backlog-frontend/src/app/add-user/add-user.component.ts
+++ backlog-frontend/src/app/add-user/add-user.component.ts
@@ -14,6 +14,7 @@ export class AddUserComponent implements OnInit {
   @Input()userID : string;
   @Input()name : string;
   @Input()avatarSrc : string;
+  statusMessage : string = "";
 
   constructor(private libService : LibraryService, private router : Router) { }
 
@@ -24,13 +25,13 @@ export class AddUserComponent implements OnInit {
     console.log("CLICKED");
     this.libService.retrieveSteamUserInfo(this.userID).subscribe(x => {
       if (x.response.players.length === 0) {
-        this.router.navigate(["user"], {queryParams: {id: this.userID}});
+        this.statusMessage = "User not found (make sure you entered your account number correctly)";
       }
       else {
         this.name = x.response.players[0].personaname;
         this.avatarSrc = x.response.players[0].avatar;
         let toAdd : User = {userID: this.userID, name: this.name, avatarSrc: this.avatarSrc};
-        console.log(toAdd);
+        this.statusMessage = "User " + toAdd.name + " found";
         this.libService.addUser(toAdd).subscribe(added => {
           this.libService.retrieveSteamUserLibrary(added.userID).subscribe(addedLib => {
             this.libService.addLibraryToDatabase(addedLib).subscribe((_) => {
